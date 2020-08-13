baec916323824b008ac9d9eb4c34321f67863fee
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-13 14:27:35 -0400

    completed all deliverables and refactored #tenure method

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,10 +2,7 @@ class Teacher < ActiveRecord::Base
   has_many :students
 
   def tenure
-    if self.years_of_experience > 5
-      true
-    else
-      false
-    end
+    self.years_of_experience > 5 ? true : false
   end
+
 end
