5e303038d489c874de7c1cdeb88491468aba01b3
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-13 16:37:53 -0400

    created grade_level class and updated modules for all cllasses. Removed teacher_id from students table and added grade_levels as a join table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,23 @@
 Student.destroy_all
 Teacher.destroy_all
+GradeLevel.destroy_all
 
-judy = Student.create(first_name: "Judy", last_name: "Smith", grade_level: "two", teacher_id: 1)
+Student.create(first_name: "Judy", last_name: "Smith", grade_level: "two")
+Student.create(first_name: "Val", last_name: "Ball", grade_level: "four")
+Student.create(first_name: "Rachael", last_name: "Ghorbani", grade_level: "twelve")
+Student.create(first_name: "Natacha", last_name: "Ngai",grade_level:"four")
+Student.create(first_name: "Toni", last_name: "Vasquez", grade_level: "two")
+Student.create(first_name: "Juan", last_name: "Mire", grade_level: "twelve")
 
-val = Student.create(first_name: "Val", last_name: "Ball", grade_level: "four", teacher_id: 2)
-rachael = Student.create(first_name: "Rachael", last_name: "Ghorbani", grade_level: "twelve",teacher_id: 3)
+Teacher.create(last_name: "Jones", grade_level: "two", years_of_experience: 20)
+Teacher.create(last_name: "Brynn", grade_level: "four", years_of_experience: 12)
+Teacher.create(last_name: "Marvin",grade_level: "twelve",years_of_experience: 4)
 
-natacha = Student.create(first_name: "Natacha", last_name: "Ngai",grade_level:"four",teacher_id: 2)
-toni = Student.create(first_name: "Toni", last_name: "Vasquez", grade_level: "two", teacher_id: 1)
-juan = Student.create(first_name: "Juan", last_name: "Mire", grade_level: "twelve", teacher_id: 3)
+GradeLevel.create(student_id: 1, teacher_id: 13)
+GradeLevel.create(student_id: 2, teacher_id: 14)
+GradeLevel.create(student_id: 3, teacher_id: 15)
+GradeLevel.create(student_id: 4, teacher_id: 14)
+GradeLevel.create(student_id: 5, teacher_id: 13)
+GradeLevel.create(student_id: 6, teacher_id: 15)
 
-jones = Teacher.create(last_name: "Jones", grade_level: "two", years_of_experience: 20)
-brynn = Teacher.create(last_name: "Brynn", grade_level: "four", years_of_experience: 12)
-marvin = Teacher.create(last_name: "Marvin",grade_level: "twelve",years_of_experience: 4)
 
